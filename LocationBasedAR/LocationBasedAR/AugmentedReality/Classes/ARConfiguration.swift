4b3cf57822141c985089e505df20317f03ff59ac
Carlos Arenas <carenasdev@gmail.com>
2018-07-25 15:29:55 -0500

    Utility functions for converting angles, normalizing degrees and getting the delta angle between two angles.

--- LocationBasedAR/LocationBasedAR/AugmentedReality/Classes/ARConfiguration.swift
+++ LocationBasedAR/LocationBasedAR/AugmentedReality/Classes/ARConfiguration.swift
@@ -17,3 +17,30 @@ let OVERLAY_VIEW_WIDTH: CGFloat = 360 * H_PIXELS_PER_DEGREE     // 360 degrees x
 let MAX_VISIBLE_ANNOTATIONS: Int = 500                          // Changing this can affect the performance
 let MAX_VERTICAL_LEVELS: Int = 10                               // Changing this can affect the performance
 
+internal func radiansToDegrees(_ radians: Double) -> Double {
+    return (radians) * (180.0 / Double.pi)
+}
+
+internal func degreesToRadians(_ degrees: Double) -> Double {
+    return (degrees) * (Double.pi / 180.0)
+}
+
+// Normalizes degree to 360
+internal func normalizeDegree(_ degree: Double) -> Double {
+    var degreeNormalized = fmod(degree, 360)
+    if degreeNormalized < 0 {
+        degreeNormalized = 360 + degreeNormalized
+    }
+    return degreeNormalized
+}
+
+// Finds the shortest angle distance between two angles. The angles must be normalized (0-360)
+internal func deltaAngle(_ angle1: Double, angle2: Double) -> Double {
+    var deltaAngle = angle1 - angle2
+    if deltaAngle > 180 {
+        deltaAngle -= 360
+    } else if deltaAngle < -180 {
+        deltaAngle += 360
+    }
+    return deltaAngle
+}
