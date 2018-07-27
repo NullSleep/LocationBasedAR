437da6e02f29cd3c02bcaccf352598107b2fc856
Carlos Arenas <carenasdev@gmail.com>
2018-07-27 15:08:43 -0500

    Setting the other tracking manager mehtods for the protocol

--- LocationBasedAR/LocationBasedAR/AugmentedReality/Classes/ARTrackingManager.swift
+++ LocationBasedAR/LocationBasedAR/AugmentedReality/Classes/ARTrackingManager.swift
@@ -12,9 +12,16 @@ import CoreLocation
 
 @objc protocol ARTrackingManagerDelegate: NSObjectProtocol {
     @objc optional func arTrackingManager(_ trackingManager: ARTrackingManager, didUpdateUserLocation location: CLLocation?)
+    @objc optional func arTrackingManager(_ trackingManager: ARTrackingManager, didUpdateReloadLocation location: CLLocation?)
+    @objc optional func arTrackingmanager(_ trackingManager: ARTrackingManager, didFailToFindLocationAfter elapsedSeconds: TimeInterval)
+    @objc optional func logText(_ text: String)
 }
 
 // Class used internally by the ARViewController for location and orientation calculations.
 open class ARTrackingManager: NSObjectProtocol, CLLocationManagerDelegate {
     
+    /**
+     Defines whether altitude is taken into account when calculating distances. Set this to falsew if your annotations don't have altitude values.
+     Note that this is only used for distance calculation, it doesn't have effect on vertical levels of annotations. Default value is false.
+     */
 }
