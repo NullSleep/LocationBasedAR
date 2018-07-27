6fb9bbb3b26260651a189171986029517706df07
Carlos Arenas <carenasdev@gmail.com>
2018-07-27 12:01:01 -0500

    More class properties and their documentation

--- LocationBasedAR/LocationBasedAR/AugmentedReality/Classes/ARViewController.swift
+++ LocationBasedAR/LocationBasedAR/AugmentedReality/Classes/ARViewController.swift
@@ -37,7 +37,7 @@ open class ARViewController: UIViewController {
      *  Defines how many vertical levels can annotations be stacked. The default value is 5.
      *  Annotations are initially vertically arranged by distance from the user, but if two annotations visible collide with each other,
      *  the farhter annotation is puth higher, meaning it is moved onto next vertical level. If annotaiton is moved onto a level higher
-     *  than this value, then it will not be visible.
+     *  than this value, then it will not be visible.å
      *  NOTE: This property greatly impacts performance because collision detection is a heavy operation, use it in range 1-10. Max value is 10.
      */
     open var maxVerticalLevel = 0 {
@@ -47,4 +47,23 @@ open class ARViewController: UIViewController {
             }
         }
     }
+    
+    // Total maximum number of visible annotation view. Default value is 100. Max value is 500.
+    open var maxVisibleAnnotations = 0 {
+        didSet {
+            if (maxVisibleAnnotations > MAX_VISIBLE_ANNOTATIONS) {
+                maxVisibleAnnotations = MAX_VISIBLE_ANNOTATIONS
+            }
+        }
+    }
+    
+    /**
+     *  The maximum distance (in meters) for an annotation to be shown.
+     *  If the distance from an annotation to the user's location is greater than this value, then that annotation will not be shown.
+     *  Also, this property, in conjunction with maxVerticalLevel, define how are annotations aligned vertically. Meaning an annotation
+     *  that are closer to this value will be higher.
+     *  Default value is 0 meters, which means that distances of annotations don't affect their visibility.
+     */
+    open var maxDistance: Double = 0
+    
 }
