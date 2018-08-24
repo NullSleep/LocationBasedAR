6f7fdbea14bff86139c285b623b6b2ccb5df62e9
Carlos Arenas <carenasdev@gmail.com>
2018-08-24 15:30:22 -0500

    Starting the locationManager delegate, implementing the method to get the location and zooming on the map

--- LocationBasedAR/Places/ViewController.swift
+++ LocationBasedAR/Places/ViewController.swift
@@ -28,9 +28,23 @@ import CoreLocation
 class ViewController: UIViewController {
   
   @IBOutlet weak var mapView: MKMapView!
+  
+  fileprivate let locationManager = CLLocationManager()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // The manager needs a delegate to notify when it has updated the position of the device.
+    locationManager.delegate = self
+    /// For desired accuracy you should alwats use the lowest accuracy that is good enough for the project's goals. This is because
+    // if we only need an accuracy of some hundred meters then the LocationManager can use phone cells and WLANs to get the position.
+    // This saves battery life, whihc you know is a big limiting factor on mobile devices. But if you need a better determination
+    // of the position, the LocationManager will use GPS, which drains the battery very fast. This is also why you should stop
+    // updating the position as soon as you have an accepatable value.
+    locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+    locationManager.startUpdatingLocation()
+    // Starts the manager and asls the user to grant permission to access location services if it was not already granted or denied.
+    locationManager.requestWhenInUseAuthorization()
   }
   
   override func didReceiveMemoryWarning() {
@@ -43,3 +57,34 @@ class ViewController: UIViewController {
   
 }
 
+extension ViewController: CLLocationManagerDelegate {
+  
+  // Every time the LocationManager, it sends this message to its delegate, giving it the updated locations. The locations array contains
+  // all locations in cronological order, so the newest location is the last object in the array.
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+
+    // Check if there are any locations in the array
+    if locations.count > 0 {
+      let location = locations.last!
+      // Get the current horizontal accuracy. This value is a radius around the current location. If yopu have a value of 50, it means
+      // that the real location can be in a circle with a radius of 50 meters around the position stored in the location
+      print("Accuarcy: \(location.horizontalAccuracy)")
+      
+      // Checking if the accuracy is high enough for our purposes. For this project 100 is good enough and we don't have to wait too
+      // long to achieve it. In industrial apps we propabluy would want an accuarcy of 10 meters or less, but in this case it could take
+      // a few minutes to achieve that accuarcy (GPS Tracking takes time)
+      // There is also a property verticalAccuracy that returns the altitude of the position. So a value of 50 means that the real altitude
+      // cab be 50 meters higher or lower. For both properties negative values are invalid.
+      if location.horizontalAccuracy < 100 {
+        
+        // Since we have the location on the accuracy that we wanted we stop updating the location to save battery life.
+        manager.stopUpdatingLocation()
+        
+        // Zooming the mapView to the location
+        let span = MKCoordinateSpan(latitudeDelta: 0.014, longitudeDelta: 0.014)
+        let region = MKCoordinateRegion(center: location.coordinate, span: span)
+        mapView.region = region
+      }
+    }
+  }
+}
