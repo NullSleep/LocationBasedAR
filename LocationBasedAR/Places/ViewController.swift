5cffaeadcfa9af75655ea53022993513761d0f21
Carlos Arenas <carenasdev@gmail.com>
2018-08-30 23:54:19 -0500

    Adding important class variables

--- LocationBasedAR/Places/ViewController.swift
+++ LocationBasedAR/Places/ViewController.swift
@@ -31,13 +31,19 @@ class ViewController: UIViewController {
   
   fileprivate let locationManager = CLLocationManager()
   
+  // Tracks if there is a request in progress, it can happen that eh CLLocationManagerDelegate method is called multiple
+  // times even after one has stopped updating the location. To avoid multiple request we use this flag.
+  fileprivate var startedLoadingPOIs = false
+  
+  // Stores the received POIs
+  fileprivate var place = [Place]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     // The manager needs a delegate to notify when it has updated the position of the device.
     locationManager.delegate = self
-    /// For desired accuracy you should alwats use the lowest accuracy that is good enough for the project's goals. This is because
-    // if we only need an accuracy of some hundred meters then the LocationManager can use phone cells and WLANs to get the position.
+    /// For desired accuracy you should alwats use the lowest accuracy that is good enough for the project's goals. This is because  if we only need an accuracy of some hundred meters then the LocationManager can use phone cells and WLANs to get the position.
     // This saves battery life, whihc you know is a big limiting factor on mobile devices. But if you need a better determination
     // of the position, the LocationManager will use GPS, which drains the battery very fast. This is also why you should stop
     // updating the position as soon as you have an accepatable value.
@@ -59,22 +65,24 @@ class ViewController: UIViewController {
 
 extension ViewController: CLLocationManagerDelegate {
   
-  // Every time the LocationManager, it sends this message to its delegate, giving it the updated locations. The locations array contains
+  // Every time the LocationManager, it sends this message to its delegate, giving it the updated locations. The
+  // locations array contains
   // all locations in cronological order, so the newest location is the last object in the array.
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
 
     // Check if there are any locations in the array
     if locations.count > 0 {
       let location = locations.last!
-      // Get the current horizontal accuracy. This value is a radius around the current location. If yopu have a value of 50, it means
+      // Get the current horizontal accuracy. This value is a radius around the current location. If yopu have a value
+      // of 50, it means
       // that the real location can be in a circle with a radius of 50 meters around the position stored in the location
       print("Accuarcy: \(location.horizontalAccuracy)")
       
-      // Checking if the accuracy is high enough for our purposes. For this project 100 is good enough and we don't have to wait too
-      // long to achieve it. In industrial apps we propabluy would want an accuarcy of 10 meters or less, but in this case it could take
-      // a few minutes to achieve that accuarcy (GPS Tracking takes time)
-      // There is also a property verticalAccuracy that returns the altitude of the position. So a value of 50 means that the real altitude
-      // cab be 50 meters higher or lower. For both properties negative values are invalid.
+      // Checking if the accuracy is high enough for our purposes. For this project 100 is good enough and we don't have
+      // to wait too long to achieve it. In industrial apps we propabluy would want an accuarcy of 10 meters or less, but
+      // in this case it could take a few minutes to achieve that accuarcy (GPS Tracking takes time).
+      // There is also a property verticalAccuracy that returns the altitude of the position. So a value of 50 means
+      // that the real altitude cab be 50 meters higher or lower. For both properties negative values are invalid.
       if location.horizontalAccuracy < 100 {
         
         // Since we have the location on the accuracy that we wanted we stop updating the location to save battery life.
